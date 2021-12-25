498860330c8ae222aad8ec8a94a8126c79a64766
marcterradas <terradas.marc@gmail.com>
2021-12-25 18:39:52 +0100

    init lang

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,10 @@
+import { useTranslation } from 'react-i18next'
+
 function App() {
+    const { t } = useTranslation()
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <h1>{t('language')}</h1>
         </div>
     )
 }
