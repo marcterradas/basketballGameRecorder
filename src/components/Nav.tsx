b3b79e3db590b8a07ca18a94cfa27d51480a11c9
marcterradas <terradas.marc@gmail.com>
2021-12-26 09:19:39 +0100

    update names

--- src/components/Nav.tsx
+++
@@ -1,5 +0,0 @@
-function Nav() {
-    return <div className="border-2 border-rose-600">Nav...</div>
-}
-
-export default Nav
---
+++ src/components/Scoreboard.tsx
@@ -0,0 +1,11 @@
+function Scoreboard() {
+    return (
+        <div className="border-2 border-rose-600">
+            <div className="team">Home Team</div>
+            <div className="timer">timer</div>
+            <div className="team">Guest Team</div>
+        </div>
+    )
+}
+
+export default Scoreboard
