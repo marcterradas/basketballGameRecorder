0235528665f83f0cceddd124e536a18344e7be56
marcterradas <terradas.marc@gmail.com>
2021-12-27 21:12:57 +0100

    add team name

--- src/components/nav/Team.tsx
+++ src/components/nav/Team.tsx
@@ -1,13 +1,14 @@
 import { Team as TeamInterface } from '../../interfaces'
 
 function Team(props: TeamInterface) {
-    console.log(props)
     const logo: string = `/src/images/${props.logo}`
+
     return (
-        <div className="team">
-            <div className="image h-40 w-40">
+        <div className="team border-2 border-rose-600">
+            <div className="image h-24 w-24">
                 <img src={logo} alt="logo" className="object-contain" />
             </div>
+            <div className="name">{props.name}</div>
         </div>
     )
 }
