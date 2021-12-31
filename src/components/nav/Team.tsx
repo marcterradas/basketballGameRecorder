8ec7ee6ec817675b3ac6fc8b53900f656898816b
marcterradas <terradas.marc@gmail.com>
2021-12-31 20:02:26 +0100

    add background center

--- src/components/nav/Team.tsx
+++ src/components/nav/Team.tsx
@@ -4,10 +4,8 @@ function Team(props: TeamInterface) {
     const logo: string = `/src/images/${props.logo}`
 
     return (
-        <div className="team border-2 border-rose-600">
-            <div className="image h-24 w-24">
-                <img src={logo} alt="logo" className="object-contain" />
-            </div>
+        <div>
+            <div className="h-40 w-40 bg-contain bg-no-repeat bg-center bg-[url('/src/images/lakers.svg')] border-2 border-rose-600"></div>
             <div className="name">{props.name}</div>
         </div>
     )
