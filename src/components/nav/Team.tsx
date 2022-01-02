078640b9f979ddba608d2ae767f2954c26851e36
marcterradas <terradas.marc@gmail.com>
2022-01-02 20:20:56 +0100

    score add bold font

--- src/components/nav/Team.tsx
+++ src/components/nav/Team.tsx
@@ -14,7 +14,7 @@ function Team(props: TeamInterface) {
                 <div className="h-40 w-40 bg-contain bg-no-repeat bg-center" style={logoStyles}></div>
                 <div className="text-center">{props.name}</div>
             </div>
-            <div className="h-40 w-40 flex items-center justify-center text-center">{score}</div>
+            <div className="bold h-40 w-40 flex items-center justify-center text-center">{score}</div>
         </div>
     )
 }
