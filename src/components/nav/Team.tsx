e1698955edf7c60e4f7bdf9f1cd8358fda15fd16
marcterradas <terradas.marc@gmail.com>
2022-01-02 07:09:11 +0100

    team container add flex

--- src/components/nav/Team.tsx
+++ src/components/nav/Team.tsx
@@ -5,11 +5,13 @@ function Team(props: TeamInterface) {
     const styles = {
         backgroundImage: `url('${logo}')`
     }
-    console.log(styles)
     return (
-        <div>
-            <div className="h-40 w-40 bg-contain bg-no-repeat bg-center border-2 border-rose-600" style={styles}></div>
-            <div className="text-center">{props.name}</div>
+        <div className="border-2 border-rose-600 flex">
+            <div>
+                <div className="h-40 w-40 bg-contain bg-no-repeat bg-center" style={styles}></div>
+                <div className="text-center">{props.name}</div>
+            </div>
+            <div>score ...</div>
         </div>
     )
 }
