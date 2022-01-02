b7206ce1ec540f4d53336fefee1898b5ffd6b520
marcterradas <terradas.marc@gmail.com>
2022-01-02 20:17:24 +0100

    add score placeholder and align

--- src/components/nav/Team.tsx
+++ src/components/nav/Team.tsx
@@ -6,13 +6,15 @@ function Team(props: TeamInterface) {
         backgroundImage: `url('${logo}')`
     }
     const containerFlexDirection: string = props.type == 'guest' ? 'flex-row-reverse' : ''
+    const score: string = '22'
+
     return (
         <div className={'flex ' + containerFlexDirection}>
             <div>
                 <div className="h-40 w-40 bg-contain bg-no-repeat bg-center" style={logoStyles}></div>
                 <div className="text-center">{props.name}</div>
             </div>
-            <div>score ...</div>
+            <div className="h-40 w-40 flex items-center justify-center text-center">{score}</div>
         </div>
     )
 }
