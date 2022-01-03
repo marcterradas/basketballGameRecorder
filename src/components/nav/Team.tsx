459b6b788227998db23223d2deb410673ca4328a
marcterradas <terradas.marc@gmail.com>
2022-01-03 20:35:18 +0100

    init bonus, possession and fouls

--- src/components/nav/Team.tsx
+++ src/components/nav/Team.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next'
+
 import { Team as TeamInterface } from '../../interfaces'
 
 function Team(props: TeamInterface) {
@@ -7,6 +9,7 @@ function Team(props: TeamInterface) {
     }
     const containerFlexDirection: string = props.type == 'guest' ? 'flex-row-reverse' : ''
     const score: string = '22'
+    const { t } = useTranslation()
 
     return (
         <div className={'flex ' + containerFlexDirection}>
@@ -15,6 +18,11 @@ function Team(props: TeamInterface) {
                 <div className="text-center">{props.name}</div>
             </div>
             <div className="bold h-40 w-40 flex items-center justify-center text-center">{score}</div>
+            <div>
+                <div>{t('bonus')}</div>
+                <div>{t('possession')}</div>
+                <div>{t('fouls')}</div>
+            </div>
         </div>
     )
 }
