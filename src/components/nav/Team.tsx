32472b3206931f8bf747f80b6f23c314b733b80f
marcterradas <terradas.marc@gmail.com>
2022-01-03 20:46:42 +0100

    update team styles

--- src/components/nav/Team.tsx
+++ src/components/nav/Team.tsx
@@ -18,10 +18,10 @@ function Team(props: TeamInterface) {
                 <div className="text-center">{props.name}</div>
             </div>
             <div className="bold h-40 w-40 flex items-center justify-center text-center">{score}</div>
-            <div>
-                <div>{t('bonus')}</div>
-                <div>{t('possession')}</div>
-                <div>{t('fouls')}</div>
+            <div className="flex flex-wrap items-center justify-center">
+                <div className="w-full">{t('bonus')}</div>
+                <div className="w-full">{t('possession')}</div>
+                <div className="w-full">{t('fouls')}</div>
             </div>
         </div>
     )
