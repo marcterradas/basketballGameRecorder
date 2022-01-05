a4c018d0482664be8fd1f9ccad67e3f01256bf7b
marcterradas <terradas.marc@gmail.com>
2022-01-05 17:34:35 +0100

    add styles logic bonus, possession and fouls

--- src/components/nav/Team.tsx
+++ src/components/nav/Team.tsx
@@ -7,9 +7,18 @@ function Team(props: TeamInterface) {
     const logoStyles: object = {
         backgroundImage: `url('${logo}')`
     }
-    const containerFlexDirection: string = props.type == 'guest' ? 'flex-row-reverse' : ''
-    const score: string = '22'
     const { t } = useTranslation()
+
+    const containerFlexDirection: string = props.type == 'guest' ? 'flex-row-reverse' : ''
+
+    const score: string = '22'
+    const bonus: boolean = false
+    const possession: boolean = false
+    const fouls: boolean = false
+
+    const bonusClass: string = bonus ? 'bg-green-600' : 'bg-red-600'
+    const possessionClass: string = possession ? 'bg-green-600' : 'bg-red-600'
+    const foulsClass: string = fouls ? 'bg-green-600' : 'bg-red-600'
 
     return (
         <div className={'flex ' + containerFlexDirection}>
@@ -20,15 +29,15 @@ function Team(props: TeamInterface) {
             <div className="bold h-40 w-40 flex items-center justify-center text-center">{score}</div>
             <div className="flex flex-wrap items-center justify-center">
                 <div className="w-full flex items-center">
-                    <div className="h-4 w-4 border-2 rounded-full border-rose-600 mr-1"></div>
+                    <div className={`h-4 w-4 border-2 rounded-full border-slate-700 mr-1 ${bonusClass}`}></div>
                     <div>{t('bonus')}</div>
                 </div>
                 <div className="w-full flex items-center">
-                    <div className="h-4 w-4 border-2 rounded-full border-rose-600 mr-1"></div>
+                    <div className={`h-4 w-4 border-2 rounded-full border-slate-700 mr-1 ${possessionClass}`}></div>
                     <div>{t('possession')}</div>
                 </div>
                 <div className="w-full flex items-center">
-                    <div className="h-4 w-4 border-2 rounded-full border-rose-600 mr-1"></div>
+                    <div className={`h-4 w-4 border-2 rounded-full border-slate-700 mr-1 ${foulsClass}`}></div>
                     <div>{t('fouls')}</div>
                 </div>
             </div>
