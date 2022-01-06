01f55747bd45e25121aff8c83b2a6befd6f06388
marcterradas <terradas.marc@gmail.com>
2022-01-06 16:22:01 +0100

    update timer styles

--- src/components/nav/Timer.tsx
+++ src/components/nav/Timer.tsx
@@ -1,10 +1,18 @@
-function Timer() {
+import { useTranslation } from 'react-i18next'
+
+function Timer(): JSX.Element {
+    const { t } = useTranslation()
+
     const timer: string = '10:00'
+    const period: number = 1
 
     return (
-        <div>
-            <div className="bold">{timer}</div>
-            <div></div>
+        <div className="w-full">
+            <div className="bold w-full text-center">{timer}</div>
+            <div className="w-full text-center">
+                <div className="bold">{period}</div>
+                <div></div>
+            </div>
         </div>
     )
 }
