a6ba0f276baf9ae75f59957388d67f801bd344ac
marcterradas <terradas.marc@gmail.com>
2022-01-06 20:17:42 +0100

    update team styles

--- src/components/nav/Team.tsx
+++ src/components/nav/Team.tsx
@@ -26,7 +26,7 @@ function Team(props: TeamInterface): JSX.Element {
                 <div className="h-40 w-40 bg-contain bg-no-repeat bg-center" style={logoStyles}></div>
                 <div className="text-center">{props.name}</div>
             </div>
-            <div className="bold h-40 w-40 flex items-center justify-center text-center">{score}</div>
+            <div className="bold h-40 w-60 flex items-center justify-center text-center text-6xl">{score}</div>
             <div className="flex flex-wrap items-center justify-center">
                 <div className="w-full flex items-center">
                     <div className={`h-4 w-4 border-2 rounded-full border-black mr-1 ${bonusClass}`}></div>
