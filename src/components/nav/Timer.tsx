07919d89f9abd89aad74f391474cb15119882e03
marcterradas <terradas.marc@gmail.com>
2022-01-07 19:59:46 +0100

    clean code

--- src/components/nav/Timer.tsx
+++ src/components/nav/Timer.tsx
@@ -5,26 +5,31 @@ function Timer(): JSX.Element {
     const { t } = useTranslation()
 
     const [start, setStart] = useState(true)
-    const [counter, setCounter] = useState(10)
+    const [time, setTime] = useState(10)
     const [period, setPeriod] = useState(1)
 
     const clickTimer = (): void => {
         setStart(!start)
     }
 
+    const updateTime = (): void => {
+        setTime(time - 1)
+    }
+
     const updateTimer = (): (() => void) | void => {
-        if (!start && counter > 0) {
-            const timer: number = setInterval(() => setCounter(counter - 1), 1000)
-            return (): void => clearInterval(timer)
+        if (!start && time > 0) {
+            // every second, update time
+            const interval: number = setInterval(updateTime, 1000)
+            return (): void => clearInterval(interval)
         }
     }
 
-    useEffect(updateTimer, [start, counter])
+    useEffect(updateTimer, [start, time])
 
     return (
         <div className="w-full">
             <div className="bold w-full text-center text-8xl pb-4 cursor-pointer select-none" onClick={clickTimer}>
-                {start ? t('start') : counter}
+                {start ? t('start') : time}
             </div>
             <div className="w-full text-center">
                 <div className="bold text-4xl select-none">{period}</div>
