ee8a14e81a0f03a42dcd772496a5faa21e82206c
marcterradas <terradas.marc@gmail.com>
2022-01-07 19:51:37 +0100

    init countdown

--- src/components/nav/Timer.tsx
+++ src/components/nav/Timer.tsx
@@ -1,22 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
 function Timer(): JSX.Element {
     const { t } = useTranslation()
 
     const [start, setStart] = useState(true)
-    const [paused, setPaused] = useState(false)
-    const [timer, setTimer] = useState('10:00')
+    const [counter, setCounter] = useState(10)
     const [period, setPeriod] = useState(1)
 
-    const updateTimer = (): void => {
-        console.log('update timer ...')
+    const clickTimer = (): void => {
+        setStart(!start)
     }
+
+    const updateTimer = () => {
+        if (!start && counter > 0) {
+            const timer: number = setInterval(() => setCounter(counter - 1), 1000)
+            return (): void => clearInterval(timer)
+        }
+    }
+
+    useEffect(updateTimer, [start, counter])
 
     return (
         <div className="w-full">
-            <div className="bold w-full text-center text-8xl pb-4 cursor-pointer select-none" onClick={updateTimer}>
-                {start ? t('start') : timer}
+            <div className="bold w-full text-center text-8xl pb-4 cursor-pointer select-none" onClick={clickTimer}>
+                {start ? t('start') : counter}
             </div>
             <div className="w-full text-center">
                 <div className="bold text-4xl select-none">{period}</div>
