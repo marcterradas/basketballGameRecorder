21f971f224e3ead79486c98d993012dca1ab13ac
marcterradas <terradas.marc@gmail.com>
2022-01-15 16:18:23 +0100

    init redux store

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,5 @@
+import { configureStore } from '@reduxjs/toolkit'
+
 import { Team as TeamInterface } from './interfaces'
 
 export const localTeam: TeamInterface = {
@@ -19,3 +21,25 @@ export const guestTeam: TeamInterface = {
     possession: false,
     fouls: false
 }
+
+const initialScoreState = { score: 0 }
+const scoreReducer = (state = initialScoreState, action: string): object => {
+    let result = {}
+
+    switch (action) {
+        case 'increase/one':
+            result = {
+                ...state,
+                score: state.score + 1
+            }
+            break
+        default:
+            result = state
+            break
+    }
+
+    return result
+}
+
+const store = configureStore({ reducer: scoreReducer })
+console.log(store.getState())
