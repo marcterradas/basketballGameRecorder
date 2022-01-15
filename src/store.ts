a6087dd314ee3c044c8a63159e7095d8ad958489
marcterradas <terradas.marc@gmail.com>
2022-01-15 15:47:42 +0100

    update localteam and guestteam default values

--- src/store.ts
+++ src/store.ts
@@ -3,11 +3,19 @@ import { Team as TeamInterface } from './interfaces'
 export const localTeam: TeamInterface = {
     type: 'local',
     name: 'Team A',
-    logo: 'lakers.svg'
+    logo: 'lakers.svg',
+    score: 0,
+    bonus: false,
+    possession: false,
+    fouls: false
 }
 
 export const guestTeam: TeamInterface = {
     type: 'guest',
     name: 'Team B',
-    logo: 'celtics.svg'
+    logo: 'celtics.svg',
+    score: 0,
+    bonus: false,
+    possession: false,
+    fouls: false
 }
