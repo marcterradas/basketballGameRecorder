14f9a0eb8c7ce10ac568d7d52fdadf31055ce614
marcterradas <terradas.marc@gmail.com>
2022-01-16 20:52:54 +0100

    change prop variable name to team

--- src/components/main/Team.tsx
+++ src/components/main/Team.tsx
@@ -1,6 +1,6 @@
 import { Team as TeamInterface } from '../../interfaces'
 
-function Team(props: TeamInterface): JSX.Element {
+function Team(team: TeamInterface): JSX.Element {
     return <div>Team ...</div>
 }
 export default Team
