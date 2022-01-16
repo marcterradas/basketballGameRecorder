644963e8a423afe58ea211c9517c6454c433a987
marcterradas <terradas.marc@gmail.com>
2022-01-16 08:24:51 +0100

    add store to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-
-import './store'
+import { Provider } from 'react-redux'
 
 import './languages/i18n'
 import './styles/index.css'
 
+import store from './store'
+
 import App from './components/App'
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root')
 )
