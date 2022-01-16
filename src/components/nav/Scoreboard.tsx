4798676ead64027c8fbe8b182a9209969d4e1f1d
marcterradas <terradas.marc@gmail.com>
2022-01-16 13:40:33 +0100

    get localTeam and guestTeam from the global state

--- src/components/nav/Scoreboard.tsx
+++ src/components/nav/Scoreboard.tsx
@@ -1,9 +1,14 @@
+import store from '../../store'
+
 import Team from './Team'
 import Timer from './Timer'
 
-import { localTeam, guestTeam } from '../../store'
+import { Team as TeamInterface } from '../../interfaces'
 
 function Scoreboard(): JSX.Element {
+    const localTeam: TeamInterface = store.getState().localTeam
+    const guestTeam: TeamInterface = store.getState().guestTeam
+
     return (
         <div className="flex items-center justify-between shadow-md pb-2">
             <Team {...localTeam}></Team>
