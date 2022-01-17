08bfac2e43166507761ea5fd9ccf293c6b4183c8
marcterradas <terradas.marc@gmail.com>
2022-01-17 20:14:27 +0100

    init logic popup

--- src/components/main/Player.tsx
+++ src/components/main/Player.tsx
@@ -1,7 +1,15 @@
 import { Player as PlayerInterface } from '../../interfaces'
 
 function Player(player: PlayerInterface): JSX.Element {
-    return <div className="h-24 w-24 rounded-full border-2 border-black flex items-center justify-center">{player.dorsal}</div>
+    const openPopup = (): void => {
+        console.log('open popup ...')
+    }
+
+    return (
+        <div className="h-24 w-24 rounded-full border-2 border-black flex items-center justify-center cursor-pointer" onClick={openPopup}>
+            {player.dorsal}
+        </div>
+    )
 }
 
 export default Player
