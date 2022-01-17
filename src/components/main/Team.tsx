2528ca9ffdddfd538c70298bc3daa7062fe2d291
marcterradas <terradas.marc@gmail.com>
2022-01-17 19:26:17 +0100

    update team style

--- src/components/main/Team.tsx
+++ src/components/main/Team.tsx
@@ -5,6 +5,6 @@ import Player from './Player'
 function Team(team: TeamInterface): JSX.Element {
     const players: Array<JSX.Element> = team.players.map((player: PlayerInterface, index: number) => <Player {...player} key={index}></Player>)
 
-    return <div>{players}</div>
+    return <div className=" grid grid-cols-4 gap-4 border-2 border-red-600">{players}</div>
 }
 export default Team
