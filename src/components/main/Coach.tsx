7f6e513ac7d86e570d83c02cc04db9fad61e87d1
marcterradas <terradas.marc@gmail.com>
2022-01-22 10:10:43 +0100

    init coach component

---
+++ src/components/main/Coach.tsx
@@ -0,0 +1,9 @@
+import { Coach as CoachInteface } from '../../interfaces'
+
+function Coach(coach: CoachInteface): JSX.Element {
+    console.log(coach)
+
+    return <div>Coach</div>
+}
+
+export default Coach
