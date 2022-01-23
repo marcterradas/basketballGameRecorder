be14a292c84072d7c3703d835a2f368c34d2ae70
marcterradas <terradas.marc@gmail.com>
2022-01-23 07:19:34 +0100

    add coach to bench array

--- src/components/main/Team.tsx
+++ src/components/main/Team.tsx
@@ -1,23 +1,25 @@
-import { Team as TeamInterface, Player as PlayerInterface, Coach as CoachInterface } from '../../interfaces'
+import { Team as TeamInterface, Player as PlayerInterface } from '../../interfaces'
 
 import Player from './Player'
 import Coach from './Coach'
 
 function Team(team: TeamInterface): JSX.Element {
     const playingPlayers: Array<JSX.Element> = []
-    const benchPlayers: Array<JSX.Element> = []
+    const bench: Array<JSX.Element> = []
+    const coach: JSX.Element = <Coach {...team.coach}></Coach>
 
     for (let index in team.players) {
         const player: PlayerInterface = team.players[index]
         const playerElement: JSX.Element = <Player {...player} key={index}></Player>
-        player.inGame ? playingPlayers.push(playerElement) : benchPlayers.push(playerElement)
+        player.inGame ? playingPlayers.push(playerElement) : bench.push(playerElement)
     }
+
+    bench.push(coach)
 
     return (
         <div>
             <div className="grid grid-cols-4 gap-4">{playingPlayers}</div>
-            <div className="grid grid-cols-4 gap-4">{benchPlayers}</div>
-            <Coach {...team.coach}></Coach>
+            <div className="grid grid-cols-4 gap-4">{bench}</div>
         </div>
     )
 }
