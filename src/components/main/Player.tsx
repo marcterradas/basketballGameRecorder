75915f2563f2fd09ec679e1e8481e90d7b49eadd
marcterradas <terradas.marc@gmail.com>
2022-01-23 13:33:55 +0100

    update player background

--- src/components/main/Player.tsx
+++ src/components/main/Player.tsx
@@ -6,7 +6,7 @@ function Player(player: PlayerInterface): JSX.Element {
     }
 
     return (
-        <div className="h-24 w-24 rounded-full border-2 shadow-lg flex items-center justify-center cursor-pointer select-none text-4xl" onClick={openPopup}>
+        <div className="h-24 w-24 rounded-full border-2 shadow-lg flex items-center justify-center cursor-pointer select-none text-4xl bg-blue-100" onClick={openPopup}>
             {player.dorsal}
         </div>
     )
