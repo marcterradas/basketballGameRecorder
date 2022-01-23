7a771559a42e90a84a56cb4553dc6f2a412cbbb4
marcterradas <terradas.marc@gmail.com>
2022-01-23 14:54:11 +0100

    create footer component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import Scoreboard from './nav/Scoreboard'
 import Main from './main/Main'
+import Footer from './footer/Footer'
 
 function App(): JSX.Element {
     return (
         <>
             <Scoreboard></Scoreboard>
             <Main></Main>
+            <Footer></Footer>
         </>
     )
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,6 @@
+
+function Footer(): JSX.Element {
+    return <div>Footer ...</div>
+}
+
+export default Footer
