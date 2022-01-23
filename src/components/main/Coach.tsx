b17966aac73eae61abdbd91c66003ccb940098c5
marcterradas <terradas.marc@gmail.com>
2022-01-23 13:33:41 +0100

    update coach background

--- src/components/main/Coach.tsx
+++ src/components/main/Coach.tsx
@@ -6,7 +6,7 @@ function Coach(coach: CoachInteface): JSX.Element {
     }
 
     return (
-        <div className="h-24 w-24 rounded-full border-2 shadow-lg flex items-center justify-center cursor-pointer select-none" onClick={openPopup}>
+        <div className="h-24 w-24 rounded-full border-2 shadow-lg flex items-center justify-center cursor-pointer select-none bg-green-100" onClick={openPopup}>
             {coach.name}
         </div>
     )
