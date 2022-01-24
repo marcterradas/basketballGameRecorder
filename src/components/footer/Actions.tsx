ba59f7e1339e333952b8f61849dfc7ed9c320882
marcterradas <terradas.marc@gmail.com>
2022-01-24 20:16:39 +0100

    init footer actions

---
+++ src/components/footer/Actions.tsx
@@ -0,0 +1,8 @@
+import { Team as TeamInterface } from '../../interfaces'
+
+function Actions(team: TeamInterface): JSX.Element {
+    console.log(team)
+    return <div>Actions ...</div>
+}
+
+export default Actions
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,19 @@
+import store from '../../store'
+
+import Actions from './Actions'
+
+import { Team as TeamInterface } from '../../interfaces'
 
 function Footer(): JSX.Element {
-    return <div className="w-full flex items-center justify-center">Footer ...</div>
+    const localTeam: TeamInterface = store.getState().localTeam
+    const guestTeam: TeamInterface = store.getState().guestTeam
+
+    return (
+        <div className="w-full flex items-center justify-center">
+            <Actions {...localTeam} ></Actions>
+            <Actions {...guestTeam}></Actions>
+        </div>
+    )
 }
 
 export default Footer
