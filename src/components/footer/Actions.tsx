abbea9f66625f6dcd3c6fe02ead9cfafbee45b02
marcterradas <terradas.marc@gmail.com>
2022-01-25 20:36:14 +0100

    update action footer styles

--- src/components/footer/Actions.tsx
+++ src/components/footer/Actions.tsx
@@ -2,7 +2,7 @@ import { Team as TeamInterface } from '../../interfaces'
 
 function Actions(team: TeamInterface): JSX.Element {
     console.log(team)
-    return <div>Actions ...</div>
+    return <div className="h-full flex items-center justify-center">Actions ...</div>
 }
 
 export default Actions
