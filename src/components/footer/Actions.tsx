c9cfde998d8b54192f2f4da8183c8ae37c74beda
marcterradas <terradas.marc@gmail.com>
2022-01-25 20:55:02 +0100

    init footer actions buttons

--- src/components/footer/Actions.tsx
+++ src/components/footer/Actions.tsx
@@ -1,8 +1,18 @@
+import { useTranslation } from 'react-i18next'
+
 import { Team as TeamInterface } from '../../interfaces'
 
 function Actions(team: TeamInterface): JSX.Element {
-    console.log(team)
-    return <div className="h-full flex items-center justify-center">Actions ...</div>
+
+    const { t } = useTranslation()
+    const containerFlexDirection: string = team.type == 'guest' ? 'flex-row-reverse' : ''
+
+    return (
+        <div className={`h-full flex items-center justify-center ${containerFlexDirection}`}>
+            <div className="h-20 w-40 border-2 shadow-lg flex items-center justify-center cursor-pointer select-none text-l">{ t('bonus') }</div>
+            <div className="h-20 w-40 border-2 shadow-lg flex items-center justify-center cursor-pointer select-none text-l">{ t('possession') }</div>
+        </div>
+    )
 }
 
 export default Actions
