490b6da62bf9f693ee58c8731d03c7691a5602f8
marcterradas <terradas.marc@gmail.com>
2022-01-25 21:10:50 +0100

    footer actions init buttons logic

--- src/components/footer/Actions.tsx
+++ src/components/footer/Actions.tsx
@@ -3,16 +3,27 @@ import { useTranslation } from 'react-i18next'
 import { Team as TeamInterface } from '../../interfaces'
 
 function Actions(team: TeamInterface): JSX.Element {
-
     const { t } = useTranslation()
     const containerFlexDirection: string = team.type == 'guest' ? 'flex-row-reverse' : ''
     const bonusMargin: string = team.type == 'local' ? 'mr-10' : ''
     const possessionMargin: string = team.type == 'guest' ? 'mr-10' : ''
 
+    const switchBonusStatus = (): void => {
+        console.log('switch bonus status ...')
+    }
+
+    const switchPossessionStatus = (): void => {
+        console.log('switch possession status ...')
+    }
+
     return (
         <div className={`h-full flex items-center justify-center ${containerFlexDirection}`}>
-            <div className={`h-16 w-40 rounded-md flex items-center justify-center cursor-pointer select-none text-l bg-green-100 ${bonusMargin}`}>{ t('bonus') }</div>
-            <div className={`h-16 w-40 rounded-md flex items-center justify-center cursor-pointer select-none text-l bg-green-100 ${possessionMargin}`}>{ t('possession') }</div>
+            <div onClick={switchBonusStatus} className={`h-16 w-40 rounded-md flex items-center justify-center cursor-pointer select-none text-l bg-green-100 ${bonusMargin}`}>
+                {t('bonus')}
+            </div>
+            <div onClick={switchPossessionStatus} className={`h-16 w-40 rounded-md flex items-center justify-center cursor-pointer select-none text-l bg-green-100 ${possessionMargin}`}>
+                {t('possession')}
+            </div>
         </div>
     )
 }
