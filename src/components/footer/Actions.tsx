26c398e562388fed7a89fadbd4285ad61b5a0128
marcterradas <terradas.marc@gmail.com>
2022-01-25 21:06:53 +0100

    add buttons margins

--- src/components/footer/Actions.tsx
+++ src/components/footer/Actions.tsx
@@ -6,11 +6,13 @@ function Actions(team: TeamInterface): JSX.Element {
 
     const { t } = useTranslation()
     const containerFlexDirection: string = team.type == 'guest' ? 'flex-row-reverse' : ''
+    const bonusMargin: string = team.type == 'local' ? 'mr-10' : ''
+    const possessionMargin: string = team.type == 'guest' ? 'mr-10' : ''
 
     return (
         <div className={`h-full flex items-center justify-center ${containerFlexDirection}`}>
-            <div className="h-20 w-40 border-2 shadow-lg flex items-center justify-center cursor-pointer select-none text-l">{ t('bonus') }</div>
-            <div className="h-20 w-40 border-2 shadow-lg flex items-center justify-center cursor-pointer select-none text-l">{ t('possession') }</div>
+            <div className={`h-16 w-40 rounded-md flex items-center justify-center cursor-pointer select-none text-l bg-green-100 ${bonusMargin}`}>{ t('bonus') }</div>
+            <div className={`h-16 w-40 rounded-md flex items-center justify-center cursor-pointer select-none text-l bg-green-100 ${possessionMargin}`}>{ t('possession') }</div>
         </div>
     )
 }
