1ae9d2103ccb0db16226addc87b1eac4e367cb5f
marcterradas <terradas.marc@gmail.com>
2022-01-30 14:59:38 +0100

    clean code

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,4 +2,6 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import timer from './timer'
 import team from './team'
 
-export default configureStore({ reducer: combineReducers({ timer, team }) })
+const reducer = combineReducers({ timer, team })
+
+export default configureStore({ reducer: reducer })
