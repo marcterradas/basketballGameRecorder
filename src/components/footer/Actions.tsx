fe2bd59e6cccbca11b47efd10e789f4b9a6819bd
marcterradas <terradas.marc@gmail.com>
2022-02-01 20:37:14 +0100

    update margins from actions

--- src/components/footer/Actions.tsx
+++ src/components/footer/Actions.tsx
@@ -8,8 +8,8 @@ import { Team as TeamInterface } from '../../interfaces'
 function Actions(team: TeamInterface): JSX.Element {
     const { t } = useTranslation()
     const containerFlexDirection: string = team.type == 'guest' ? 'flex-row-reverse' : ''
-    const bonusMargin: string = team.type == 'local' ? 'mr-10' : ''
-    const possessionMargin: string = team.type == 'guest' ? 'mr-10' : ''
+    const bonusMargin: string = team.type == 'local' ? 'mr-8' : ''
+    const possessionMargin: string = team.type == 'guest' ? 'mr-8' : ''
 
     const switchBonusStatus = (): void => {
         store.dispatch({ type: 'switchBonusStatus', team: team.type })
@@ -20,11 +20,11 @@ function Actions(team: TeamInterface): JSX.Element {
     }
 
     return (
-        <div className={`h-full flex items-center justify-center ${containerFlexDirection}`}>
-            <div onClick={switchBonusStatus} className={`h-16 w-40 rounded-md flex items-center justify-center cursor-pointer select-none text-l bg-green-100 ${bonusMargin}`}>
+        <div className={`h-full flex items-center justify-center ${containerFlexDirection} px-6`}>
+            <div onClick={switchBonusStatus} className={`h-16 w-36 rounded-md flex items-center justify-center cursor-pointer select-none text-l bg-green-200 ${bonusMargin}`}>
                 {t('bonus')}
             </div>
-            <div onClick={switchPossessionStatus} className={`h-16 w-40 rounded-md flex items-center justify-center cursor-pointer select-none text-l bg-green-100 ${possessionMargin}`}>
+            <div onClick={switchPossessionStatus} className={`h-16 w-36 rounded-md flex items-center justify-center cursor-pointer select-none text-l bg-green-200 ${possessionMargin}`}>
                 {t('possession')}
             </div>
         </div>
