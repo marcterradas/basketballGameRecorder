bbf0e464f61d1a5171c792bcade22ccc5c8cc61f
marcterradas <terradas.marc@gmail.com>
2022-02-04 16:36:19 +0100

    init language select

--- src/components/footer/Language.tsx
+++ src/components/footer/Language.tsx
@@ -1,13 +1,20 @@
-
-import {languages} from '../../languages/i18n'
-console.log(languages)
+import { languages } from '../../languages/i18n'
+import { Language as LanguageInterface } from '../../interfaces'
 
 function Language(): JSX.Element {
+    const languageList: Array<JSX.Element> = []
 
-    return (
-        <div></div>
-    )
+    for (let languageKey in languages) {
+        const language: LanguageInterface = languages[languageKey]
+        const languageElement: JSX.Element = (
+            <option key={language.key} value={language.value}>
+                {language.value}
+            </option>
+        )
+        languageList.push(languageElement)
+    }
 
+    return <select>{languageList}</select>
 }
 
 export default Language
