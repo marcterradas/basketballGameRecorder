7c03f8a357b62419e4f47725ec91258c9ab7d7ab
marcterradas <terradas.marc@gmail.com>
2022-02-04 16:13:50 +0100

    export languages

--- src/languages/i18n.ts
+++ src/languages/i18n.ts
@@ -3,4 +3,10 @@ import { initReactI18next } from 'react-i18next'
 
 import translations from './translations.json'
 
+export const languages = {
+    es: 'Castellà',
+    ca: 'Català',
+    en: 'English'
+}
+
 i18n.use(initReactI18next).init(translations)
