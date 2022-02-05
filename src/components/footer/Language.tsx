20780d6fd04930060849cf1f4b5580a7e0506fce
marcterradas <terradas.marc@gmail.com>
2022-02-05 16:27:54 +0100

    handle change language

--- src/components/footer/Language.tsx
+++ src/components/footer/Language.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react'
+
 import { languages } from '../../languages/i18n'
 import { Language as LanguageInterface } from '../../interfaces'
 
 function Language(): JSX.Element {
     const languageList: Array<JSX.Element> = []
+    const [selectedOption, setSelectedOption] = useState('Català')
 
     for (let languageKey in languages) {
         const language: LanguageInterface = languages[languageKey]
@@ -14,7 +17,15 @@ function Language(): JSX.Element {
         languageList.push(languageElement)
     }
 
-    return <select className="h-14 w-24 rounded-md text-l text-center cursor-pointer bg-blue-200">{languageList}</select>
+    const changeLanguage = (event: any): void => {
+        setSelectedOption(event.target.value)
+    }
+
+    return (
+        <select value={selectedOption} onChange={(event) => changeLanguage(event)} className="h-14 w-24 rounded-md text-l text-center cursor-pointer bg-blue-200">
+            {languageList}
+        </select>
+    )
 }
 
 export default Language
