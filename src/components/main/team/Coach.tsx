4347463d6a89db393aee95b7f2dd6ce8a1270eab
marcterradas <terradas.marc@gmail.com>
2022-02-06 12:32:42 +0100

    update coach styles

--- src/components/main/team/Coach.tsx
+++ src/components/main/team/Coach.tsx
@@ -1,6 +1,5 @@
 import store from '../../../store/store'
 
-
 import { Coach as CoachInteface } from '../../../interfaces'
 
 function Coach(coach: CoachInteface): JSX.Element {
@@ -9,7 +8,7 @@ function Coach(coach: CoachInteface): JSX.Element {
     }
 
     return (
-        <div className="h-24 w-24 rounded-full border-2 shadow-lg flex items-center justify-center cursor-pointer select-none bg-green-200" onClick={openPopup}>
+        <div className="h-24 w-24 rounded-full border-2 shadow-lg flex items-center justify-center cursor-pointer select-none border-stone-500 font-bold" onClick={openPopup}>
             {coach.name}
         </div>
     )
