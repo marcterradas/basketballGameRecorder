e00f53952fdb5c166447a05633d82a19df53c9b9
marcterradas <terradas.marc@gmail.com>
2022-02-09 21:36:13 +0100

    init handle show popups

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -9,6 +9,12 @@ function Main(): JSX.Element {
     const localTeam: TeamInterface = store.getState().team.localTeam
     const guestTeam: TeamInterface = store.getState().team.guestTeam
 
+    const showPopups = (): void => {
+        console.log('handeShowPupups ...')
+    }
+
+    store.subscribe(showPopups)
+
     return (
         <div className="w-full flex items-stretch justify-between pt-4 px-6 border-y-2">
             <Team {...localTeam}></Team>
