88cf4b32e2c12071a73b2e4cead328600e0a73a0
marcterradas <terradas.marc@gmail.com>
2022-02-10 16:18:59 +0100

    remove popup logic from main

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -8,19 +8,6 @@ import { Team as TeamInterface } from '../../interfaces'
 function Main(): JSX.Element {
     const localTeam: TeamInterface = store.getState().team.localTeam
     const guestTeam: TeamInterface = store.getState().team.guestTeam
-    let showPlayerPopup: boolean = false
-    let showCoachPopup: boolean = false
-    let selectedPlayerId: number | null = null
-    let selectedCoachId: number | null = null
-
-    const showPopups = (): void => {
-        showPlayerPopup = store.getState().showPlayerPopup
-        showCoachPopup = store.getState().showCoachPopup
-        selectedPlayerId = showPlayerPopup ? store.getState().selectedPlayerId : null
-        selectedCoachId = showCoachPopup ? store.getState().selectedCoachId : null
-    }
-
-    store.subscribe(showPopups)
 
     return (
         <div className="w-full flex items-stretch justify-between pt-4 px-6 border-y-2">
