25e535128d2f686d1296a6295b5b87327d0d4bbf
marcterradas <terradas.marc@gmail.com>
2022-02-10 16:19:13 +0100

    add popup logic to app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,26 @@
+import store from '../store/store'
+
+
 import Scoreboard from './nav/Scoreboard'
 import Main from './main/Main'
 import Footer from './footer/Footer'
 
 function App(): JSX.Element {
+
+    let showPlayerPopup: boolean = false
+    let showCoachPopup: boolean = false
+    let selectedPlayerId: number | null = null
+    let selectedCoachId: number | null = null
+
+    const showPopups = (): void => {
+        showPlayerPopup = store.getState().showPlayerPopup
+        showCoachPopup = store.getState().showCoachPopup
+        selectedPlayerId = showPlayerPopup ? store.getState().selectedPlayerId : null
+        selectedCoachId = showCoachPopup ? store.getState().selectedCoachId : null
+    }
+
+    store.subscribe(showPopups)
+
     return (
         <div className="flex flex-wrap min-h-full">
             <Scoreboard></Scoreboard>
