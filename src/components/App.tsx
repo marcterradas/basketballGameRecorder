cce9eaeda24cc63cf35a91d5836f733664361a06
marcterradas <terradas.marc@gmail.com>
2022-02-10 17:10:43 +0100

    init popup logic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,19 @@
 import store from '../store/store'
 
+import Coach from './popup/Coach'
+import Player from './popup/Player'
 
 import Scoreboard from './nav/Scoreboard'
 import Main from './main/Main'
 import Footer from './footer/Footer'
 
 function App(): JSX.Element {
-
     let showPlayerPopup: boolean = false
     let showCoachPopup: boolean = false
     let selectedPlayerId: number | null = null
     let selectedCoachId: number | null = null
+
+    let popup: JSX.Element = <div></div>
 
     const showPopups = (): void => {
         showPlayerPopup = store.getState().showPlayerPopup
@@ -23,6 +26,7 @@ function App(): JSX.Element {
 
     return (
         <div className="flex flex-wrap min-h-full">
+            {popup}
             <Scoreboard></Scoreboard>
             <Main></Main>
             <Footer></Footer>
