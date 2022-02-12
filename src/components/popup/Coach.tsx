5685e23b9abcb29d19e1b3b82478ecb640d2e922
marcterradas <terradas.marc@gmail.com>
2022-02-12 14:54:51 +0100

    init coach popup

--- src/components/popup/Coach.tsx
+++ src/components/popup/Coach.tsx
@@ -1,5 +1,15 @@
+import store from '../../store/store'
+
 function Coach(): JSX.Element {
-    return <div className="z-2 absolute w-full h-full border-2 border-red-600 flex items-center justify-center">Coach popup ...</div>
+    const closePopup = (): void => {
+        store.dispatch({ type: 'closePopup' })
+    }
+
+    return (
+        <div onClick={closePopup} className="z-2 absolute w-full h-full bg-gray-400 opacity-80 flex items-center justify-center">
+            Coach popup ...
+        </div>
+    )
 }
 
 export default Coach
