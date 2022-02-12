e6066e01536501b37d3af57b944d0294d315114e
marcterradas <terradas.marc@gmail.com>
2022-02-12 10:31:14 +0100

    init coach popup

--- src/components/popup/Coach.tsx
+++ src/components/popup/Coach.tsx
@@ -1,5 +1,5 @@
 function Coach(): JSX.Element {
-    return <div></div>
+    return <div className="z-2 absolute w-full h-full border-2 border-red-600 flex items-center justify-center">Coach popup ...</div>
 }
 
 export default Coach
