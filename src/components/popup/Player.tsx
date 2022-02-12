59e1f912e430b2d4565f83f5eb95d4cb1321bdfc
marcterradas <terradas.marc@gmail.com>
2022-02-12 14:54:41 +0100

    init player popup

--- src/components/popup/Player.tsx
+++ src/components/popup/Player.tsx
@@ -1,5 +1,15 @@
+import store from '../../store/store'
+
 function Player(): JSX.Element {
-    return <div className="z-2 absolute w-full h-full border-2 border-red-600 flex items-center justify-center">Player Popup ...</div>
+    const closePopup = (): void => {
+        store.dispatch({ type: 'closePopup' })
+    }
+
+    return (
+        <div onClick={closePopup} className="z-2 absolute w-full h-full bg-gray-400 opacity-80 flex items-center justify-center">
+            Player Popup ...
+        </div>
+    )
 }
 
 export default Player
