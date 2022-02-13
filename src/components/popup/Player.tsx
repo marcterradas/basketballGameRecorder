c9019e08807ee3c1fa0e1ebd0ac48141f03baca7
marcterradas <terradas.marc@gmail.com>
2022-02-13 12:03:04 +0100

    create close button

--- src/components/popup/Player.tsx
+++ src/components/popup/Player.tsx
@@ -5,9 +5,14 @@ function Player(): JSX.Element {
         store.dispatch({ type: 'closePopup' })
     }
 
+    const styles: object = {
+        backgroundImage: "url('/src/images/close.png')"
+    }
+
     return (
-        <div onClick={closePopup} className="z-2 absolute w-full h-full bg-gray-400 opacity-80 flex items-center justify-center">
-            Player Popup ...
+        <div className="z-2 absolute w-full h-full bg-gray-400 opacity-90 flex items-center justify-center">
+            <div onClick={closePopup} className="absolute top-4 right-4 cursor-pointer w-12 h-12 bg-contain bg-no-repeat bg-center" style={styles}></div>
+            <div className="z-3">Player popup ...</div>
         </div>
     )
 }
