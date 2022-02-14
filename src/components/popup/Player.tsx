7ebe608a2ae5d81fb7ec0b92f69c6ad9f1e9307f
marcterradas <terradas.marc@gmail.com>
2022-02-14 20:02:23 +0100

    popup player, init logic add points

--- src/components/popup/Player.tsx
+++ src/components/popup/Player.tsx
@@ -1,18 +1,33 @@
 import store from '../../store/store'
 
 function Player(): JSX.Element {
+    const styles: object = {
+        backgroundImage: "url('/src/images/close.png')"
+    }
+
     const closePopup = (): void => {
         store.dispatch({ type: 'closePopup' })
     }
 
-    const styles: object = {
-        backgroundImage: "url('/src/images/close.png')"
+    const addPoint = (points: number): void => {
+        console.log(points)
+        closePopup()
     }
 
     return (
         <div className="z-2 absolute w-full h-full bg-gray-400 opacity-90 flex items-center justify-center">
             <div onClick={closePopup} className="absolute top-4 right-4 cursor-pointer w-12 h-12 bg-contain bg-no-repeat bg-center" style={styles}></div>
-            <div className="z-3">Player popup ...</div>
+            <div className="z-3">
+                <div onClick={() => addPoint(1)} className="cursor-pointer">
+                    Add one point
+                </div>
+                <div onClick={() => addPoint(2)} className="cursor-pointer">
+                    Add two points
+                </div>
+                <div onClick={() => addPoint(3)} className="cursor-pointer">
+                    Add three points
+                </div>
+            </div>
         </div>
     )
 }
