4004a618c0de7f06b2b5e8c3a55807b99911b9f6
marcterradas <terradas.marc@gmail.com>
2022-02-19 12:16:45 +0100

    player popup

--- src/components/popup/Player.tsx
+++ src/components/popup/Player.tsx
@@ -22,13 +22,13 @@ function Player(): JSX.Element {
         <div className="z-2 absolute w-full h-full bg-gray-400 opacity-90 flex items-center justify-center">
             <div onClick={closePopup} className="absolute top-4 right-4 cursor-pointer w-12 h-12 bg-contain bg-no-repeat bg-center" style={styles}></div>
             <div className="z-3">
-                <div onClick={() => addPoint(1)} className="cursor-pointer">
+                <div onClick={() => addPoint(1)} className="mb-4 h-14 w-32 rounded-md flex items-center justify-center cursor-pointer select-none text-l border-2 border-stone-500 font-bold bg-white">
                     {t('add_one_point')}
                 </div>
-                <div onClick={() => addPoint(2)} className="cursor-pointer">
+                <div onClick={() => addPoint(2)} className="mb-4 h-14 w-32 rounded-md flex items-center justify-center cursor-pointer select-none text-l border-2 border-stone-500 font-bold bg-white">
                     {t('add_two_points')}
                 </div>
-                <div onClick={() => addPoint(3)} className="cursor-pointer">
+                <div onClick={() => addPoint(3)} className="h-14 w-32 rounded-md flex items-center justify-center cursor-pointer select-none text-l border-2 border-stone-500 font-bold bg-white">
                     {t('add_three_points')}
                 </div>
             </div>
