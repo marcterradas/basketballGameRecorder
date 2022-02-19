aa0e9904b6a0efec687e821d1a2e71a0d095adb0
marcterradas <terradas.marc@gmail.com>
2022-02-19 13:38:54 +0100

    init historic store

---
+++ src/store/modules/historic.ts
@@ -0,0 +1,13 @@
+import { Historic } from '../../interfaces'
+
+const historic: Array<Historic> = []
+
+const initialScoreState = {
+    historic
+}
+
+const reducer = (state = initialScoreState, action: any) => {
+    return state
+}
+
+export default reducer
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,8 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import timer from './modules/timer'
 import team from './modules/team'
+import historic from './modules/historic'
 
-const reducer = combineReducers({ timer, team })
+const reducer = combineReducers({ timer, team, historic })
 
 export default configureStore({ reducer: reducer })
