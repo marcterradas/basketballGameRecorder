40bee526ce436d754cf8534e23b3528159a910fb
marcterradas <terradas.marc@gmail.com>
2022-02-21 21:39:32 +0100

    fix performance rendering

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,37 +1,12 @@
-import { useState } from 'react'
-
-import store from '../store/store'
-
-import Coach from './popup/Coach'
-import Player from './popup/Player'
-
 import Scoreboard from './nav/Scoreboard'
 import Main from './main/Main'
 import Footer from './footer/Footer'
+import Popup from './popup/Main'
 
 function App(): JSX.Element {
-    const [popup, setPopup] = useState(<div></div>)
-
-    const showPopups = (): void => {
-        const selectedPlayerId: number | null = store.getState().team.selectedPlayerId
-        const selectedCoachId: number | null = store.getState().team.selectedCoachId
-
-        setPopup(<div></div>)
-
-        if (selectedPlayerId) {
-            setPopup(<Player></Player>)
-        }
-
-        if (selectedCoachId) {
-            setPopup(<Coach></Coach>)
-        }
-    }
-
-    store.subscribe(showPopups)
-
     return (
         <div className="flex flex-wrap min-h-full">
-            {popup}
+            <Popup></Popup>
             <Scoreboard></Scoreboard>
             <Main></Main>
             <Footer></Footer>
--- src/components/popup/Main.tsx
+++ src/components/popup/Main.tsx
@@ -1,5 +1,31 @@
+import { useState } from 'react'
+
+import store from '../../store/store'
+
+import Coach from './Coach'
+import Player from './Player'
+
 function Main(): JSX.Element {
-    return <div></div>
+    const [popup, setPopup] = useState(<div></div>)
+
+    const showPopups = (): void => {
+        const selectedPlayerId: number | null = store.getState().team.selectedPlayerId
+        const selectedCoachId: number | null = store.getState().team.selectedCoachId
+
+        setPopup(<div></div>)
+
+        if (selectedPlayerId) {
+            setPopup(<Player></Player>)
+        }
+
+        if (selectedCoachId) {
+            setPopup(<Coach></Coach>)
+        }
+    }
+
+    store.subscribe(showPopups)
+
+    return <>{popup}</>
 }
 
 export default Main
