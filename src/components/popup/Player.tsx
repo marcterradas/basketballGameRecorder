5779eb4746d0d229fa43fea8c3222433eaf61d2f
marcterradas <terradas.marc@gmail.com>
2022-02-27 20:21:41 +0100

    player popup, add cursor styles logic with countdown

--- src/components/popup/Player.tsx
+++ src/components/popup/Player.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next'
 function Player(props: any): JSX.Element {
     const { t } = useTranslation()
 
-    const styles: object = {
+    const closeButtonStyles: object = {
         backgroundImage: "url('/src/images/close.png')"
     }
 
@@ -16,21 +16,23 @@ function Player(props: any): JSX.Element {
     const addPoint = (points: number): void => {
         if (props.inCountDown) {
             store.dispatch({ type: 'addPoints', points: points })
+            closePopup()
         }
-        closePopup()
     }
+
+    const customButton: string = props.inCountDown ? 'cursor-pointer' : 'cursor-not-allowed'
 
     return (
         <div className="z-2 absolute w-full h-full bg-gray-400 opacity-90 flex items-center justify-center">
-            <div onClick={closePopup} className="absolute top-4 right-4 cursor-pointer w-12 h-12 bg-contain bg-no-repeat bg-center" style={styles}></div>
+            <div onClick={closePopup} className="absolute top-4 right-4 cursor-pointer w-12 h-12 bg-contain bg-no-repeat bg-center" style={closeButtonStyles}></div>
             <div className="z-3">
-                <div onClick={() => addPoint(1)} className="mb-4 h-14 w-32 rounded-md flex items-center justify-center cursor-pointer select-none text-l border-2 border-stone-500 font-bold bg-white">
+                <div onClick={() => addPoint(1)} className={`mb-4 h-14 w-32 rounded-md flex items-center justify-center select-none text-l border-2 border-stone-500 font-bold bg-white ${customButton}`}>
                     {t('add_one_point')}
                 </div>
-                <div onClick={() => addPoint(2)} className="mb-4 h-14 w-32 rounded-md flex items-center justify-center cursor-pointer select-none text-l border-2 border-stone-500 font-bold bg-white">
+                <div onClick={() => addPoint(2)} className={`mb-4 h-14 w-32 rounded-md flex items-center justify-center select-none text-l border-2 border-stone-500 font-bold bg-white ${customButton}`}>
                     {t('add_two_points')}
                 </div>
-                <div onClick={() => addPoint(3)} className="h-14 w-32 rounded-md flex items-center justify-center cursor-pointer select-none text-l border-2 border-stone-500 font-bold bg-white">
+                <div onClick={() => addPoint(3)} className={`h-14 w-32 rounded-md flex items-center justify-center select-none text-l border-2 border-stone-500 font-bold bg-white ${customButton}`}>
                     {t('add_three_points')}
                 </div>
             </div>
