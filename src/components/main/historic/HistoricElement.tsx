91ed0e771f4a0b2c420c3eb9b9679906e9f5a6d5
marcterradas <terradas.marc@gmail.com>
2022-03-05 19:08:40 +0100

    get player and team from store

--- src/components/main/historic/HistoricElement.tsx
+++ src/components/main/historic/HistoricElement.tsx
@@ -1,5 +1,13 @@
+import store from '../../../store/store'
+
+import { Player, Team } from '../../../interfaces'
 
 function HistoricElement(historic: any): JSX.Element {
+    const team: Team = store.getState().team.localTeam.id == historic.teamId ? store.getState().team.localTeam : store.getState().team.guestTeam
+    const player: Player = typeof store.getState().team.localTeam.players[historic.playerId] !== 'undefined' ? store.getState().team.localTeam.players[historic.playerId] : store.getState().team.guestTeam.players[historic.playerId]
+
+    console.log({ team: team, player: player })
+
     return <div></div>
 }
 
