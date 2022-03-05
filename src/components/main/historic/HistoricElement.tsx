d8d0b79e50b3f6694527fc29b9eab52161ff55e4
marcterradas <terradas.marc@gmail.com>
2022-03-05 19:49:25 +0100

    update historic element styles

--- src/components/main/historic/HistoricElement.tsx
+++ src/components/main/historic/HistoricElement.tsx
@@ -6,9 +6,15 @@ function HistoricElement(historic: any): JSX.Element {
     const team: Team = store.getState().team.localTeam.id == historic.teamId ? store.getState().team.localTeam : store.getState().team.guestTeam
     const player: Player = typeof store.getState().team.localTeam.players[historic.playerId] !== 'undefined' ? store.getState().team.localTeam.players[historic.playerId] : store.getState().team.guestTeam.players[historic.playerId]
 
-    console.log({ team: team, player: player })
+    const flexDirection: string = team.type == 'guest' ? 'flex-row-reverse' : ''
+    console.log(team)
 
-    return <div></div>
+    return (
+        <div className={`w-full border border-red-600 flex ${flexDirection}`}>
+            <div>{player.dorsal}</div>
+            <div>{player.name}</div>
+        </div>
+    )
 }
 
 export default HistoricElement
