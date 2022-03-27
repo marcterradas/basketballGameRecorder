616ecee6218fcaf9502a57662f12ed7d549d50ab
marcterradas <terradas.marc@gmail.com>
2022-03-27 18:22:20 +0200

    fix z-index

--- src/components/popup/Player.tsx
+++ src/components/popup/Player.tsx
@@ -32,9 +32,9 @@ function Player(props: any): JSX.Element {
     const customButton: string = props.inCountDown ? 'cursor-pointer' : 'cursor-not-allowed'
 
     return (
-        <div className="z-2 absolute w-full h-full bg-gray-400 opacity-90 flex items-center justify-center">
+        <div className="z-10 absolute w-full h-full bg-gray-400 opacity-90 flex items-center justify-center">
             <div onClick={closePopup} className="absolute top-4 right-4 cursor-pointer w-12 h-12 bg-contain bg-no-repeat bg-center" style={closeButtonStyles}></div>
-            <div className="z-3">
+            <div className="z-10">
                 <div onClick={() => addPoint(1)} className={`mb-4 h-14 w-32 rounded-md flex items-center justify-center select-none text-l border-2 border-stone-500 font-bold bg-white ${customButton}`}>
                     {t('add_one_point')}
                 </div>
